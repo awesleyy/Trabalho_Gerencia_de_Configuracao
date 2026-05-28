e5b65dc118338ecee25383449c756c07add4a962
lfariazzz <levi.farias@aluno.ufca.edu.br>
2026-05-28 19:45:04 -0300

    feat(models): cria modulo de perfil

---
+++ backend/src/models/perfil.py
@@ -0,0 +1,7 @@
+class perfil():
+    def __init__(self, nome, idade, email, senha):
+        self.nome = nome
+        self.idade = idade
+        self.email = email
+        self.senha = senha
+    
