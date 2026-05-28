4c6c26b4ad68b401694d1dfff8fa816c7a0e6eda
lfariazzz <levi.farias@aluno.ufca.edu.br>
2026-05-28 19:45:52 -0300

    feat(perfil): implementa funcao de editar usuario

--- backend/src/models/perfil.py
+++ backend/src/models/perfil.py
@@ -4,4 +4,14 @@ class perfil():
         self.idade = idade
         self.email = email
         self.senha = senha
-    
+
+    def editar_usuario(self):
+        nome = str(input("Digite o seu nome: "))
+        self.nome = nome
+        idade = str(input("Digite a sua idade: "))
+        self.idade = idade
+        email = str(input("Digite o seu email: "))
+        self.email = email
+        senha = str(input("Digite a sua senha: "))
+        self.senha = senha
+
