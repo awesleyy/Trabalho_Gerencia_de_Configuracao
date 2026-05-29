22ab9b5e43fb6a8e97ce4950d7e5bd5a15c58226
Henrique Coimbra <henrique.coimbra@aluno.ufca.edu.br>
2026-05-29 19:27:36 -0300

    Imports da main colocados

---
+++ main.py
@@ -0,0 +1,4 @@
+from backend.src.models.cliente import Cliente 
+from backend.src.models.perfil import Perfil 
+from backend.src.models.carrinho import Carrinho 
+from backend.src.models.produto import Produto 
